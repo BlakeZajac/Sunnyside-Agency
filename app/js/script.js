e13529ebd044a4f581e722c485670b6bf66c2de8
BlakeZajac <contact@blakezajac.com>
2022-04-10 22:21:11 +1000

    Added Overlay and fadeIn/Out.

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,25 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.hasFade'); 
 
 btnHamburger.addEventListener('click', function(){
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){ // Close Menu
+        body.classList.remove('noScroll');
         header.classList.remove('open');
+        fadeElements.forEach(function(element){
+            element.classList.remove('fadeIn');
+            element.classList.add('fadeOut');
+        })
     }
-    else {
+    else { // Open Menu
+        body.classList.add('noScroll');
         header.classList.add('open');
+        fadeElements.forEach(function(element){
+            element.classList.remove('fadeOut');
+            element.classList.add('fadeIn');
+        })
     }
 });
